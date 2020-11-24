8a26b67be1df5bd4a73674fccab75526bb23d818
jlayher <john.e.layher@gmail.com>
2020-11-24 03:53:47 -0500

    Changes to variable names in app, minor changes to pug templates

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const data = require('./data.json');
+const {projects} = require('./data.json');
 
 const app = express();
 
@@ -8,7 +8,7 @@ app.use('/static', express.static('public'));
 
 //root route
 app.get('/', (req, res) => {
-    res.render('index', {data: data.projects});
+    res.render('index', {projects});
 }); 
 
 //about route
@@ -18,9 +18,9 @@ app.get('/about', (req, res) =>{
 
 //dynamic project route
 //issues here with the project routes
-app.get('/project/:id', (req, res) => {
+app.get(`/project/:id`, (req, res) => {
     const projectId = req.params.id;
-    const project = data.projects.find( ({id}) => id === +projectId);
+    const project = projects.find( ({id}) => id === +projectId);
 
     res.render(`project/${id}`, {project});
 });
