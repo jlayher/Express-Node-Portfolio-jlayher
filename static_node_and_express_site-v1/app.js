8e127ddd45596eff55756def5a67023d4e4c4a59
jlayher <john.e.layher@gmail.com>
2020-12-10 20:58:49 -0500

    lots of changes.  Not sure if they good ones :'(

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const {projects} = require('./data.json');
-
 const app = express();
+const {projects} = require('./data.json');
+const path = require('path');
 
 app.set('view engine', 'pug');
-app.use('/static', express.static('public'));
+// app.use('/static', express.static('public'));
+// app.use('/images', express.static('images'));
+
+app.use("/static", express.static(path.join(__dirname, "/public")));
+app.use("/images", express.static(path.join(__dirname, "/images/")));
 
 //root route
 app.get('/', (req, res) => {
@@ -16,13 +20,45 @@ app.get('/about', (req, res) =>{
     res.render('about');
 });
 
+//error route
+app.get('/error', (req, res) => {
+    res.render('error');
+});
+
+//page-not-found route
+app.get('/page-not-found', (req, res) => {
+    res.render('page-not-found');
+});
+
 //dynamic project route
 //issues here with the project routes
 app.get('/project/:id', (req, res) => {
     const {id} = req.params;
     const project = projects[id];
-
+    console.log(project);
     res.render('project', {project});
+});
+
+//Handle 404 Erros
+app.use((req, res, next) => {
+    const err = new Error();
+    err.status = 404;
+    next(err);
+});
+
+//Global Error Handler
+app.use((err, req, res, next) =>{
+    if (err.status === 400) {
+        console.log("404 Error.  Page Not Found");
+        res.locals.message = "An Error Occured!  The Page Wasn't found!";
+        res.status(404);
+        res.render('error', {err});
+    } else {
+        const err = new Error();
+    
+
+    }
+
 });
 
 app.listen(3000, ()=>{
