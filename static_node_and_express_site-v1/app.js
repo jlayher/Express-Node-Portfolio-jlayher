307552844bdc21efbd737ddced0f6d0fe1d4e8af
jlayher <john.e.layher@gmail.com>
2020-12-13 03:38:15 -0500

    Got Error handlers working, and styled anchor tags

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -5,12 +5,8 @@ const path = require('path');
 
 app.set('view engine', 'pug');
 
-//this or the stuff below
-//app.use('/static', express.static('public'));
-//app.use('/images', express.static('images'));
-
-app.use("/static", express.static(path.join(__dirname, "/public")));
-app.use("/images", express.static(path.join(__dirname, "/images/")));
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //root route
 app.get('/', (req, res) => {
@@ -33,34 +29,37 @@ app.get('/page-not-found', (req, res) => {
 });
 
 //dynamic project route
-//issues here with the project routes
 app.get('/project/:id', (req, res) => {
     const {id} = req.params;
     const project = projects[id];
-    console.log(project);
-    res.render('project', {project});
+    if (project){
+        res.render('project', {project});
+    } else {
+        res.redirect('/views/page-not-found');
+    }  
 });
 
 //Handle 404 Erros
 app.use((req, res, next) => {
-    const err = new Error();
+    const err = new Error("404 Page Not Found");
     err.status = 404;
     next(err);
 });
 
 //Global Error Handler
 app.use((err, req, res, next) =>{
-    if (err.status === 400) {
+    if (err.status === 404) {
         console.log("404 Error.  Page Not Found");
-        res.locals.message = "An Error Occured!  The Page Wasn't found!";
+        err.message = "An Error Occured!  The Page Wasn't found!";
         res.status(404);
-        res.render('error', {err});
+        res.render('page-not-found', {err});
     } else {
-        const err = new Error();
-    
-
+        console.log("A Server Error Occurred! -__- Try Returning to the Home Page!");
+        err.message = `Looks Like a Server Error Ocurred \\('0')/`;
+        err.status = 500 || err.status;
+        res.status(500);
+        res.render('error', {err});
     }
-
 });
 
 app.listen(3000, ()=>{
